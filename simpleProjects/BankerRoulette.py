ed7194cce794b8cf4a4f180a0845bc4b02d64f01
SuzzZain <127651501+SuzzZain@users.noreply.github.com>
2024-06-27 21:51:03 +0545

    Banker Roulette, who will pay.

--- simpleProjects/BankerRoulette.py
+++ simpleProjects/BankerRoulette.py
@@ -1,7 +1,7 @@
 names_string = "Alice,Bob,Charlie,David"
-names = names_string.split(",")
+names = names_string.split(", ")
 
 import random
 num_items = len(names)
 random_choice = random.randint(0, num_items -1)
-print(names[random_choice])
+print(names[random_choice] + " is going to buy the meal today!")
