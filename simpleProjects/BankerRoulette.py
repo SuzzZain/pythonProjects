0f679431574cfe50945db7706059cea0da0fdf67
SuzzZain <127651501+SuzzZain@users.noreply.github.com>
2024-06-27 21:34:59 +0545

    Banker Roulette, who will pay

---
+++ simpleProjects/BankerRoulette.py
@@ -0,0 +1,7 @@
+names_string = "Alice,Bob,Charlie,David"
+names = names_string.split(",")
+
+import random
+num_items = len(names)
+random_choice = random.randint(0, num_items -1)
+print(names[random_choice])
